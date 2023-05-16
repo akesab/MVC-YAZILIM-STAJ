1844c61a4be5b05d777dd0b54074f402374795a0
akesab <65190737+akesab@users.noreply.github.com>
2023-05-16 19:06:35 +0300

    ürün yönetim sistemi staj 2.proje

---
+++ urun_kayıt_sistemi/urun_kayıt_sistemi/Controllers/UrunController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using urun_kayıt_sistemi.Models;
+
+namespace urun_kayıt_sistemi.Controllers
+{
+    public class UrunController : Controller
+    {
+        private urunEntities db = new urunEntities();
+
+        // GET: Urun
+        public ActionResult Index() //listelendiği alan
+        {
+            return View(db.Table.ToList());
+        }
+
+        // GET: Urun/Details/5
+        public ActionResult Details(int? id) //id bazında detay veri getirme
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // GET: Urun/Create
+        public ActionResult Create() //oluşturma alanı
+        {
+            return View();
+        }
+
+        // POST: Urun/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UrunId,UrunAdi,UrunKodu,UrunUrl,Urunfiyat")] Table table) 
+        {
+            if (ModelState.IsValid)
+            {
+                db.Table.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        // GET: Urun/Edit/5
+        public ActionResult Edit(int? id) //düzenleme alanı
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Urun/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UrunId,UrunAdi,UrunKodu,UrunUrl,Urunfiyat")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Urun/Delete/5
+        public ActionResult Delete(int? id) //silme alanı
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Urun/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Table.Find(id);
+            db.Table.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
